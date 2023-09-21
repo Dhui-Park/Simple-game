001334ae46b8cf3c631da1b35e382724d0b3506e
dhuiPark <hasong925@gmail.com>
2023-09-21 13:42:15 +0900

    Initial Commit

---
+++ Simple_game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple_game
+//
+//  Created by dhui on 2023/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
